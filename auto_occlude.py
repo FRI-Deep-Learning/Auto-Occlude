d579a5824577ac1717ccc825d4179d2082ad1983
Layne Gustafson <laynegustafson97@gmail.com>
2017-05-04 22:10:54 -0400

    Made it work. Must be run from a directory in which the IMFDB_FIXED directory resides

--- auto_occlude.py
+++ auto_occlude.py
@@ -7,12 +7,12 @@ IMG_SIZE = 64
 MAX_SIZE = 25
 MIN_SIZE = 15
 # Remember to modify this!
-ROOT_DIR = ""
+ROOT_DIR = "./IMFDB_FIXED"
 
-def random_occlude(name):
-	img = cv2.imread(name, 3)
-	cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-	cv2.imshow('image', img)
+def random_occlude(dirName, name):
+	img = cv2.imread(dirName + "/" + name, 1)
+	# cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+	# cv2.imshow('image', img)
 
 
 	width = random.randrange(MIN_SIZE, MAX_SIZE)
@@ -22,17 +22,17 @@ def random_occlude(name):
 	y = random.randrange(1, IMG_SIZE-MAX_SIZE)
 
 	# Python: cv2.rectangle(img, pt1, pt2, color[, thickness[, lineType[, shift]]])
-	occlude = cv2.rectangle(img, (x, y), (x + width, y + height), 0, -1)
+	cv2.rectangle(img, (x, y), (x + width, y + height), 0, -1)
 
-	cv2.imshow('image', occlude)
-	cv2.imwrite('occluded-'+name, occlude)
+	cv2.imwrite(dirName + '/occluded-'+name, img)
 
 
 def auto_occlude():
 	for subdir, dirs, files in os.walk(ROOT_DIR):
 		for file in files:
-			print(file)
-			random_occlude(file)
+			if file.startswith("mod_") and file.endswith(".jpg"):
+				random_occlude(subdir, file)
+				print(subdir + "/" + file)
 
 auto_occlude()
 
